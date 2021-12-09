3308bd26f836f83349e8b17fddc6e7be66455cb7
AYUSH YADAV <ayushexx@gmail.com>
2021-12-09 15:58:59 +0530

    feat: homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import {Link} from 'react-router-dom';
+
+const { Title } = Typography;
 
 function Homepage() {
-    return (
-        <div>
-            homepage
-        </div>
-    )
+  return <>
+        <Title level={2} className="heading">Global Crypto Stats</Title>
+        <Row>
+            <Col span={12}><Statistic title="Total Crptocurrency"/></Col>
+            <Col span={12}><Statistic title="Total Exchanges"/></Col>
+            <Col span={12}><Statistic title="Total Market Cap"/></Col>
+            <Col span={12}><Statistic title="Total 24h Volume"/></Col>
+            <Col span={12}><Statistic title="Total Markets"/></Col>
+        </Row>
+  </>;
 }
 
 export default Homepage;
